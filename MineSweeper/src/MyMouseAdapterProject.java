4f414bbe75a5c8e1b66205b3c1e5d64acb946c96
Dylan Hernandez <Dylan Hernandez @10.32.63.103>
2017-03-27 15:51:35 -0400

    COMMIT CHECK

--- MineSweeper/src/MyMouseAdapterProject.java
+++ MineSweeper/src/MyMouseAdapterProject.java
@@ -9,9 +9,6 @@ import javax.swing.JFrame;
 
 public class MyMouseAdapterProject extends MouseAdapter {
 	
-	
-
-	
 	public void mousePressed(MouseEvent e) {
 		switch (e.getButton()) {
 		case 1:		//Left mouse button
@@ -37,7 +34,26 @@ public class MyMouseAdapterProject extends MouseAdapter {
 			myPanel.repaint();
 			break;
 		case 3:		//Right mouse button
-			//Do nothing
+			Component c = e.getComponent();
+			while (!(c instanceof JFrame)) {
+				c = c.getParent();
+				if (c == null) {
+					return;
+				}
+			}
+			JFrame myFrame = (JFrame) c;
+			MyPanelProject myPanel = (MyPanelProject) myFrame.getContentPane().getComponent(0);
+			Insets myInsets = myFrame.getInsets();
+			int x1 = myInsets.left;
+			int y1 = myInsets.top;
+			e.translatePoint(-x1, -y1);
+			int x = e.getX();
+			int y = e.getY();
+			myPanel.x = x;
+			myPanel.y = y;
+			myPanel.mouseDownGridX = myPanel.getGridX(x, y);
+			myPanel.mouseDownGridY = myPanel.getGridY(x, y);
+			myPanel.repaint();
 			break;
 		default:    //Some other button (2 = Middle mouse button, etc.)
 			//Do nothing
