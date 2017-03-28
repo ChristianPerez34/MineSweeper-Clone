3bf79efcee25becd7b8eda8c10cb1581aabf6f80
Christian Perez Villanueva <craze@DESKTOP-PSF52HH>
2017-03-28 15:19:54 -0400

    Adjusted frame size

--- MineSweeper/src/MainGame.java
+++ MineSweeper/src/MainGame.java
@@ -11,8 +11,8 @@ public class MainGame {
 	public static void main(String[] args) {
 		JFrame myFrame = new JFrame("Minesweeper Game");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(500, 150);
-		myFrame.setSize(600, 600);
+		myFrame.setSize(350, 350);
+		myFrame.setLocationRelativeTo(null);
 
 		MineSweeperGrid myPanel = new MineSweeperGrid();
 		myFrame.add(myPanel);
